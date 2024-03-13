ca9aa6383a86ad8c4e022159bd51f241b29dbad2
GabrielGP <globezno2004@gmail.com>
2024-03-13 10:03:39 +0100

    changes in users

--- app/routes/UserRoutes.py
+++ app/routes/UserRoutes.py
@@ -9,7 +9,7 @@ from app.middlewares.AuthMiddleware import check_auth
 from app.utils.Roles import UserRole
 
 users = Blueprint('users', __name__, url_prefix='/users')
-admin = Blueprint('admin', __name__, url_prefix='/admin')
+adminUsers = Blueprint('admin', __name__, url_prefix='/admin/users')
 
 repo = MongoRepository('users')
 
@@ -68,36 +68,19 @@ def delete_me():
 @users.route('/', methods=['GET'])
 @check_auth(UserRole.USER)
 def get_users():
-    users = repo.get_all()
-    users_list = [user for user in users]
-    return jsonify(users_list), 200
+    # solo se devuelven los usuarios que no son privados, y solo se devuelven los campos necesarios (username, profile_picture)
+    users = repo.get_by_query_with_projection({'private': False}, {'username': 1, 'profile_picture': 1})
+
+    return Response(dumps(users), status=200, mimetype='application/json')
 
 
-@users.route('/<id>', methods=['GET'])
+@users.route('/<string:id>', methods=['GET'])
 @check_auth(UserRole.USER)
 def get_user(id):
-    query = {'_id': id}
-        
-    projection = {
-        '_id': 0,
-        'username': 1,
-        'email': 1,
-        'name': 1,
-        'gender': 1,
-        'birthdate': 1,
-        'profile_picture': 1,
-        'bio': 1,
-        'links': 1,
-        'private': 1,
-        'vip': 1,
-        'followers': 1,
-        'body_measures': 1,
-        'training_programs': 1,
-        'following': 1
-    }    
-    user = repo.get_by_query_with_projection(query=query, projection=projection)
     
-    if not user:
+    user = repo.get_one(id)
+    
+    if not user or user['private']:
         return jsonify({'error': 'User not found'}), 404
     
     return Response(dumps(user), status=200, mimetype='application/json')
@@ -106,80 +89,34 @@ def get_user(id):
 
 # **** ADMIN *****
 
-@admin.route('/me', methods=['GET'])
-@check_auth(UserRole.USER)
-def me():
-    user = repo.get_one(request.user_id)
-    return Response(dumps(user), mimetype='application/json')
 
-@admin.route('/me', methods=['PUT'])
-@check_auth(UserRole.USER)
-def update_me():
-    data = request.get_json()
-    try:
-        validate(instance=data, schema=userSchema)
-    except Exception as e:
-        return jsonify({'error': 'Invalid data'}), 400
-
-    user = repo.get_one(request.user_id)
-    hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
-
-    user['username'] = escape(data['username'])
-    user['email'] = escape(data['email'])
-    user['password'] = hashed_password
-    user['name'] = escape(data['name'])
-    user['gender'] = data['gender']
-    user['birthdate'] = data['birthdate']
-    user['profile_picture'] = escape(data['profile_picture'])
-    user['bio'] = escape(data['bio'])
-    user['links'] = data['links']
-    user['private'] = data['private']
-    user['vip'] = data['vip']
-    user['followers'] = data['followers']
-    user['body_measures'] = data['body_measures']
-    user['training_programs'] = data['training_programs']
-    user['following'] = data['following']
-
-    repo.update(request.user_id, user)
-    return jsonify({'success': True}), 200
-
-@admin.route('/me', methods=['DELETE'])
-@check_auth(UserRole.USER)
-def delete_me():
-    repo.delete(request.user_id)
-    return jsonify({'success': True}), 200
-
-@admin.route('/', methods=['GET'])
-@check_auth(UserRole.USER)
+@adminUsers.route('/', methods=['GET'])
+@check_auth(UserRole.ADMIN)
 def get_users():
     users = repo.get_all()
-    users_list = [user for user in users]
-    return jsonify(users_list), 200
 
-@admin.route('/<id>', methods=['GET'])
-@check_auth(UserRole.USER)
+    return Response(dumps(users), status=200, mimetype='application/json')
+
+
+@adminUsers.route('/<string:id>', methods=['GET'])
+@check_auth(UserRole.ADMIN)
 def get_user(id):
-    query = {'_id': id}  
-    projection = {
-        '_id': 0,
-        'username': 1,
-        'email': 1,
-        'name': 1,
-        'gender': 1,
-        'birthdate': 1,
-        'profile_picture': 1,
-        'bio': 1,
-        'links': 1,
-        'private': 1,
-        'vip': 1,
-        'followers': 1,
-        'body_measures': 1,
-        'training_programs': 1,
-        'following': 1
-    }    
-    user = repo.get_by_query_with_projection(query=query, projection=projection)
+    user = repo.get_one(id)
+
     if not user:
         return jsonify({'error': 'User not found'}), 404
+
     return Response(dumps(user), status=200, mimetype='application/json')
+
+
+
+@adminUsers.route('/<string:id>', methods=['DELETE'])
+@check_auth(UserRole.ADMIN)
+def delete_user(id):
+    repo.delete(id)
+
+    return jsonify({'success': True}), 200
+
+
 
 
